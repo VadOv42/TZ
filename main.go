d20334a058b24f90d27a27b9f22d2b254fe08241
Vadim Shalnev <shalnev.vadik@yandex.ru>
2023-04-06 19:22:42 +0400

    finalv0.1.1

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func input(a, operand, b string) string {
 	var res string
 	num1, err1 := strconv.Atoi(a)
 	num2, err2 := strconv.Atoi(b)
+	// преобразует в число и считает
 	if err1 == nil && err2 == nil {
 		if num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10 {
 			fmt.Println("Пожалуйста, используйте два целых числа от 1 до 10")
@@ -33,40 +34,42 @@ func input(a, operand, b string) string {
 				fmt.Println(f(num1, num2))
 			}
 		}
-
+		// создает карту, ищет значение по ключу
 	} else if err1 != nil && err2 != nil {
 		var carta = map[string]int{
 			"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
 			"XX": 20, "XXX": 30, "XL": 40, "L": 50, "LX": 60, "LXX": 70, "LXXX": 80, "XC": 90, "C": 100}
-		var arcarta int
-		var arcartab int
-		arcarta = carta[a]
-		arcartab = carta[b]
+		var arcart_a int
+		var arcarta_b int
+		arcart_a = carta[a]
+		arcarta_b = carta[b]
 
-		if arcarta < 1 {
+		if arcart_a < 1 {
 			fmt.Println("Пожалуйста, используйте два целых числа от I до X")
-		} else if arcartab > 10 {
+		} else if arcarta_b > 10 {
 			fmt.Println("Пожалуйста, используйте два целых числа от I до X")
-		} else if arcartab < 1 {
+		} else if arcarta_b < 1 {
 			fmt.Println("Пожалуйста, используйте два целых числа от I до X")
-		} else if arcarta > 10 {
+		} else if arcart_a > 10 {
 			fmt.Println("Пожалуйста, используйте два целых числа от I до X")
 		} else {
 			var ress int
-			var arcarta int
-			var arcartab int
+			var arcart_a int
+			var arcarta_b int
+			fmt.Println(ress)
+			// считает
 			for key := range carta {
-				arcarta = carta[a]
-				arcartab = carta[b]
+				arcart_a = carta[a]
+				arcarta_b = carta[b]
 				key = key
 				if operand == "+" {
-					ress = arcarta + arcartab
+					ress = arcart_a + arcarta_b
 				} else if operand == "-" {
-					ress = arcarta - arcartab
+					ress = arcart_a - arcarta_b
 				} else if operand == "*" {
-					ress = arcarta * arcartab
+					ress = arcart_a * arcarta_b
 				} else if operand == "/" {
-					ress = arcarta / arcartab
+					ress = arcart_a / arcarta_b
 				}
 
 			}
@@ -77,7 +80,7 @@ func input(a, operand, b string) string {
 			sip = ress - res
 			var keys string
 			var keyss string
-
+			// результат обратно в строку
 			for key, i := range carta {
 				if i == sip {
 					keys = key
@@ -92,6 +95,7 @@ func input(a, operand, b string) string {
 
 			result := keys + keyss
 			fmt.Println(result)
+
 		}
 	}
 	return res
